3b2643f07810686c4a44be9f42822d1faff622a7
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2021-03-31 01:22:38 +0200

    Adds short sanity test for strong type interconvertibility

--- codingame/ghost-in-the-cell/tests/gitc_test.cpp
+++ codingame/ghost-in-the-cell/tests/gitc_test.cpp
@@ -2,5 +2,15 @@
 
 #include "ghost_in_the_cell.hpp"
 
+using namespace gitc;
 
-TEST(Dummy, DoesNothing) {}
+TEST(Types, ConvertToAndFromDouble) {
+  strength test{10};
+  ASSERT_EQ(test * 1.3, 13);
+}
+
+TEST(Types, AreInterconvertibleWhenSpecified) {
+  duration d{4};
+  production_capacity p{3};
+  ASSERT_EQ(d * p, strength{12});
+}
