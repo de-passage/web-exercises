7ee37e57da63f5d56181a49f638a5126ffbec6ff
Sylvain Leclercq <maisbiensurqueoui@gmail.com>
2021-04-19 18:07:09 +0200

    Implements simulator logic except orders

--- codingame/ghost-in-the-cell/simulator/simulator.cpp
+++ codingame/ghost-in-the-cell/simulator/simulator.cpp
@@ -29,23 +29,23 @@ struct player_info {
 struct running_t {
 } constexpr static run;
 struct end {
-  player winner;
+  owner_type winner;
 };
+constexpr static end draw{owner_type::neutral};
 struct forfeit {
   enum class type { invalid_action } reason;
-  player winner;
+  owner_type winner;
 };
 
 struct status {
   enum class type : char { running, ended, forfeited };
 
-  constexpr status() : _what{}, _running{} {}
-  constexpr explicit status(end winner) : _what(type::ended), _ended{winner} {}
-  constexpr explicit status(forfeit winner)
-      : _what{type::forfeited}, _forfeit{winner} {}
+  constexpr status(running_t) : _what{}, _running{} {}
+  constexpr status(end winner) : _what(type::ended), _ended{winner} {}
+  constexpr status(forfeit winner) : _what{type::forfeited}, _forfeit{winner} {}
 
   constexpr type what() const { return _what; }
-  constexpr player winner() const {
+  constexpr owner_type winner() const {
     if (_what == type::ended) {
       return _ended.winner;
     }
@@ -69,19 +69,23 @@ class game {
   game() = default;
 
   status play_one_turn(const decision_list& player_decisions) {
-    std::vector<troop_id> troops_at_destination;
-    std::vector<bomb_id> bombs_at_destination;
+    std::vector<troop_container::iterator> troops_at_destination;
+    std::vector<bomb_container::iterator> bombs_at_destination;
 
-    for (auto& troop : _troops) {
-      --troop.second.distance;
-      if (troop.second.distance == 0) {
-        troops_at_destination.push_back(troop.first);
+    // 1. Move troops
+    for (auto troop_it = _troops.begin(); troop_it != _troops.end();
+         ++troop_it) {
+      auto& troop = troop_it->second;
+      --troop.distance;
+      if (troop.distance == 0) {
+        troops_at_destination.push_back(troop_it);
       }
     }
-    for (auto& bomb : _bombs) {
-      --bomb.second.distance;
-      if (bomb.second.distance == 0) {
-        bombs_at_destination.push_back(bomb.first);
+    for (auto bomb_it = _bombs.begin(); bomb_it != _bombs.end(); ++bomb_it) {
+      auto& bomb = bomb_it->second;
+      --bomb.distance;
+      if (bomb.distance == 0) {
+        bombs_at_destination.push_back(bomb_it);
       }
     }
 
@@ -95,23 +99,126 @@ class game {
 
     // 3. Produce new cyborgs
     for (auto& factory : _factories) {
-      factory.second.cyborgs +=
-          (factory.second.owner == owner_type::neutral
-               ? strength{0}
-               : strength{factory.second.production.value});
+      if (factory.second.inactivity > 0) {
+        --factory.second.inactivity;
+      }
+      else {
+        factory.second.cyborgs +=
+            (factory.second.owner == owner_type::neutral
+                 ? strength{0}
+                 : strength{factory.second.production.value});
+      }
     }
 
     // 4. resolve fights
-    for (auto& troop : troops_at_destination) {
+    struct fight {
+      strength me_troops{};
+      strength opponent_troops{};
+    };
+    std::unordered_map<factory_id, fight> fights;
+
+    // Aggregate different troops reaching potentially same factories, and
+    // remove them from the list
+    for (auto troop_it : troops_at_destination) {
+      auto& troop = troop_it->second;
+      if (troop.owner == owner_type::me) {
+        fights[troop.target].me_troops += troop.cyborgs;
+      }
+      else {
+        fights[troop.target].opponent_troops += troop.cyborgs;
+      }
+
+      _troops.erase(troop_it);
+    }
+    // Resolve fights factory by factory. Newcomers first, then against defenses
+    for (auto& fight : fights) {
+      owner_type winner = owner_type::neutral;
+      strength leftover{};
+      if (fight.second.me_troops > fight.second.opponent_troops) {
+        leftover = fight.second.me_troops - fight.second.opponent_troops;
+        winner = owner_type::me;
+      }
+      else {
+        leftover = fight.second.opponent_troops - fight.second.me_troops;
+        winner = owner_type::opponent;
+      }
+
+      auto factory_it = _factories.find(fight.first);
+      if (factory_it == _factories.end()) {
+        throw std::logic_error("Fighting over a factory that doesn't exist");
+      }
+
+      auto& factory = factory_it->second;
+      if (winner == factory.owner) {
+        factory.cyborgs += leftover;
+      }
+      else if (factory.cyborgs >= leftover) {
+        factory.cyborgs -= leftover;
+      }
+      else {
+        factory.cyborgs = leftover - factory.cyborgs;
+        factory.owner = winner;
+      }
     }
 
     // 5. bombs explode
-    for (auto& bombs : bombs_at_destination) {
+    for (auto& bomb_it : bombs_at_destination) {
+      auto& bomb = bomb_it->second;
+
+      auto factory_it = _factories.find(bomb.target);
+      if (factory_it == _factories.end()) {
+        throw std::logic_error("Trying to bomb a factory that doesn't exist.");
+      }
+      auto& factory = factory_it->second;
+
+      auto destroyed = std::max(factory.cyborgs / 2, strength{10});
+      factory.cyborgs = factory.cyborgs >= destroyed
+                            ? factory.cyborgs - destroyed
+                            : strength{0};
+      factory.inactivity = duration{5};
+
+      _bombs.erase(bomb_it);
     }
 
     // 6. Check for game end
     // If a player has 0 factories and 0 troops, he loses. otherwise if we reach
     // turn 200, the player with the most cyborgs wins
+    strength by_player[2]{strength{0}, strength{0}};
+    for (auto& troop : _troops) {
+      if (troop.second.owner == owner_type::me) {
+        by_player[0] += troop.second.cyborgs;
+      }
+      else if (troop.second.owner == owner_type::opponent) {
+        by_player[1] += troop.second.cyborgs;
+      }
+    }
+    for (auto& factory : _factories) {
+      if (factory.second.owner == owner_type::me) {
+        by_player[0] += factory.second.cyborgs;
+      }
+      else if (factory.second.owner == owner_type::opponent) {
+        by_player[1] += factory.second.cyborgs;
+      }
+    }
+
+    if (by_player[0] == 0) {
+      return end{owner_type::opponent};
+    }
+    if (by_player[1] == 0) {
+      return end{owner_type::me};
+    }
+
+    if (++_turn == 200) {
+      if (by_player[0] > by_player[1]) {
+        return end{owner_type::me};
+      }
+      if (by_player[0] < by_player[1]) {
+        return end{owner_type::opponent};
+      }
+      return draw;
+    }
+
+    return run;
   }
 
  private:
@@ -119,6 +226,7 @@ class game {
   factory_container _factories{};
   bomb_container _bombs{};
   player_info players[2]{};
+  int _turn{0};
 };
 
 }  // namespace simulator
